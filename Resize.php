62098e7627878ca17b754129643877424acdb652
Alexnguyen17 <Alexnguyen170395@gmail.com>
2018-11-02 19:11:35 +0700

    hhh

---
+++ Resize.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alexnguyen
+ * Date: 02/11/2018
+ * Time: 17:58
+ */
+
+
+interface Resizeable
+{
+    public function resize($percent);
+}
+?>
+</body>
+</html>
